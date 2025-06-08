f6831bd9f606657b0c3fa51f10070b98dca17a68
th1sf <thialisonsilvaf@gmail.com>
2025-06-08 18:10:56 -0300

    Add files via upload

---
+++ 004.py
@@ -0,0 +1,13 @@
+#Escreva um programa que converta real para o Franco Congolês
+
+#Leitura de dados
+
+reais = float(input('Digite o valor a ser convertido: '))
+
+#Calculo para conversão
+
+conversao = reais * 496.26
+
+#Retorno do resultado
+
+print(f'{reais} reais, equilavem a {conversao} Franco Congoleses')
