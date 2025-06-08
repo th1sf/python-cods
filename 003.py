b1920093b8d39c2dc37c0e6eaa2e042f5a407965
th1sf <thialisonsilvaf@gmail.com>
2025-06-08 18:10:36 -0300

    Add files via upload

---
+++ 003.py
@@ -0,0 +1,9 @@
+#Escreva um programa que leia o nome, depois o sobrenome, e retorne para o usuário
+
+#Leitura de dados
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+
+#Retorno do resultado
+
+print(f'Seu nome completo é {nome} {sobrenome}')
