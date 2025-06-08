525ded9bbed65a22e59b3e08a5c50f0fd6f2b5a5
th1sf <thialisonsilvaf@gmail.com>
2025-06-08 18:09:21 -0300

    Add files via upload

---
+++ 002.py
@@ -0,0 +1,10 @@
+#Escreva um programa que leia, o nome, idade, e cidade de nascimento e retorne para o usuário
+
+#Leitura de dados
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+cidade_nasc = input('Digite sua cidade de nascimento: ')
+
+#Retorno das informações solicitadas
+
+print(f'Seu nome é {nome}, sua idade é {idade} e sua cidade de nascimento é {cidade_nasc}')
