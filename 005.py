d289433a16098e983a1395f61f30c99d0c6caaa5
th1sf <thialisonsilvaf@gmail.com>
2025-06-08 18:11:16 -0300

    Add files via upload

---
+++ 005.py
@@ -0,0 +1,18 @@
+#Escreva um programa que leia o raio de uma esfera, e calcule o seu volume e área.
+
+#leitura de dados
+
+raio = float(input('Digite o volume: '))
+
+
+#Calculo
+
+V = (4/3) * 3.1415 * raio ** 3
+A = 4 * 3.1415 * raio ** 2
+
+#Resultado
+
+print(f'Volume da Esfera: {V:.2f}\nA area é: {A:.2f}')
+
+
+
